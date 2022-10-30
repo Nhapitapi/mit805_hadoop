d215c0b7e13cf63dc8de0dd7f500b4b72c14167a
Nhapitapi <91605572+Nhapitapi@users.noreply.github.com>
2022-10-30 23:03:25 +0200

    Add files via upload

---
+++ SalesByBrand.py
@@ -0,0 +1,19 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class SalesAnalsyer(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_ratings,
+                   reducer=self.reducer_count_ratings)
+        ]
+
+    def mapper_get_sales(self, _, line):
+        (event_time, event_type, product_id, category_id, category_code, brand, price, user_id) = line.split('\t')
+        yield brand, 1
+
+    def reducer_count_sales(self, key, values):
+        yield key, sum(values)
+
+if __name__ == '__main__':
+    SalesAnalyser.run()
